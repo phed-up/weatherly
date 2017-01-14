54c38facd2e3b34b6d6653894de17a78d44c63e8
Sandy Wong <fed_up@hotmail.com>
2017-01-14 02:14:54 -0600

    Initial Commit

---
+++ weatherly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherly
+//
+//  Created by skwong on 1/14/17.
+//  Copyright © 2017 fedup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
